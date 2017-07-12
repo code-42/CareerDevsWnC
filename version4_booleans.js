012e87bb637264631cf9dee64132a437a7ec9b09
Edward Dupre <edwd42@gmail.com>
2017-07-12 19:29:31 +0000

    todoList.toggleCompleted should flip the completed property

--- version4_booleans.js
+++ version4_booleans.js
@@ -115,3 +115,84 @@ length
 __proto__
 :
 Array(0)
+
+
+// todoList.toggleCompleted should flip the completed property
+// add a new method called toggleCompleted
+var todoList = {
+  todos: [],
+  displayTodos: function(){
+    console.log('My Todos', this.todos);
+  },
+  addTodo: function(todoText){
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
+    this.displayTodos();
+  },
+// change parameter newValue to todoText
+  changeTodo: function(position, todoText){
+// change this.todos[position] = newValue; to a property on the object
+    this.todos[position].todoText = todoText;
+    this.displayTodos();
+  },
+  deleteTodo: function(position){
+    this.todos.splice(position,1);
+    this.displayTodos();
+  },
+// add a new method called toggleCompleted
+// use ! to get the opposite of a boolean value
+  toggleCompleted: function(position){
+      var todo = this.todos[position];
+      todo.completed = !todo.completed;
+      this.displayTodos();
+  }
+};
+
+
+
+// run it
+todoList.addTodo('boolean testing');
+VM2407 script.js:1 My Todos 
+[Object]
+0
+:
+Object
+completed
+:
+false
+todoText
+:
+"boolean testing"
+__proto__
+:
+Object
+length
+:
+1
+__proto__
+:
+Array(0)
+undefined
+todoList.toggleCompleted(0);
+VM2407 script.js:1 My Todos 
+[Object]
+0
+:
+Object
+completed
+:
+true
+todoText
+:
+"boolean testing"
+__proto__
+:
+Object
+length
+:
+1
+__proto__
+:
+Array(0)
