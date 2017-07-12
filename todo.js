cd950f4d9645a628bb3a3aff481cfa37d988aede
ED <edwd42@gmail.com>
2017-07-12 19:38:33 -0400

    Update todo.js
    cleanup after merge

--- todo.js
+++ todo.js
@@ -46,7 +46,7 @@ var todoList = {
   }
 };
 */
-<<<<<<< HEAD
+
 
 // version 4 - booleans
 // todoList.toggleCompleted should flip the completed property
@@ -79,31 +79,5 @@ var todoList = {
       var todo = this.todos[position];
       todo.completed = !todo.completed;
       this.displayTodos();
-  }
-};
-=======
->>>>>>> v4-booleans
-
-// version 4
-// change the addTodo method so it adds objects instead of text to the todos array
-var todoList = {
-  todos: [],
-  displayTodos: function(){
-    console.log('My Todos', this.todos);
-  },
-  addTodo: function(todoText){
-    this.todos.push({
-      todoText: todoText,
-      completed: false
-    });
-    this.displayTodos();
-  },
-  changeTodo: function(position, newValue){
-    this.todos[position] = newValue;
-    this.displayTodos();
-  },
-  deleteTodo: function(position){
-    this.todos.splice(position,1);
-    this.displayTodos();
   }
 };
