0516629573a2cd528a8c0f6de5ba4e065c5fcc0a
Edward Dupre <edwd42@gmail.com>
2017-07-12 19:06:24 +0000

    todoList.changeTodo should change the todoText property

--- version4_booleans.js
+++ version4_booleans.js
@@ -22,6 +22,96 @@ var todoList = {
   }
 };
 
+// run it
+todoList.addTodo('this is an object');
+VM2407 script.js:1 My Todos 
+[Object]
+0
+:
+Object
+completed
+:
+false
+todoText
+:
+"this is an object"
+__proto__
+:
+Object
+length
+:
+1
+__proto__
+:
+Array(0)
 
 
+// todoList.changeTodo should change the todoText property
+var todoList = {
+  todos: [],
+  displayTodos: function(){
+    console.log('My Todos', this.todos);
+  },
+  addTodo: function(todoText){
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
+    this.displayTodos();
+  },
+  // change parameter newValue to todoText
+  changeTodo: function(position, todoText){
+    // change this.todos[position] = newValue; to a property on the object
+    this.todos[position].todoText = todoText;
+    this.displayTodos();
+  },
+  deleteTodo: function(position){
+    this.todos.splice(position,1);
+    this.displayTodos();
+  }
+};
 
+// run it
+todoList.addTodo('first try');
+VM2407 script.js:1 My Todos 
+[Object]
+0
+:
+Object
+completed
+:
+false
+todoText
+:
+"first try"
+__proto__
+:
+Object
+length
+:
+1
+__proto__
+:
+Array(0)
+undefined
+todoList.changeTodo(0,'second try');
+VM2407 script.js:1 My Todos 
+[Object]
+0
+:
+Object
+completed
+:
+false
+todoText
+:
+"second try"
+__proto__
+:
+Object
+length
+:
+1
+__proto__
+:
+Array(0)
