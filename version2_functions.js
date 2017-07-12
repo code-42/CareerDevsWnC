e572f53c09aed11b9ffc96a726715fc347db6473
Edward Dupre <edwd42@gmail.com>
2017-07-12 03:20:05 +0000

    finished Requirements

---
+++ version2_functions.js
@@ -0,0 +1,42 @@
+makeTurkeySandwich
+    Get one slice of bread.
+    Add turkey.
+    Put a slice of bread on top.
+    
+function makeTurkeySandwich(){
+    Get one slice of bread;
+    Add turkey;
+    Put a slice of bread on top;
+}
+
+makeTurkeySandwich()
+
+makeSandwichWith _____
+    Get one slice of bread.
+    Add ______.
+    Put a slice of bread on top.
+    
+function makeSandwichWith(filling){
+    Get one slice of bread;
+    Add filling;
+    Put a slice of bread on top;
+}    
+
+makeSandwichWith _ham_
+makeSandwichWith('ham')
+
+console.log('My Todos', todos)
+
+function sayHiTo(person){
+    console.log('hi', person);
+}
+
+sayHiTo('gordon')  // hi gordon
+
+V2 Requirements
+It should have a function to:
+display todos
+add todos
+change todos
+delete todos
+
