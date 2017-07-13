fdd8d03d613ea446645797c0b5c6910577cdf877
Edward Dupre <edwd42@gmail.com>
2017-07-13 11:20:56 +0000

    commit todo.js before checkout v4

--- todo.js
+++ todo.js
@@ -1,56 +1,7 @@
-/*
-// version 2 - functions 
-var todos = ['item 1', 'item 2', 'item 3']
+var todos = ['item 1', 'item 2', 'item 3'];
 
-// it should have a function to display todos
-function displayTodos(){
-    console.log('My todos:', todos);
-}
-
-// it should have a function to add todos
-function addTodo(todo){
-    todos.push(todo);
-    displayTodos();
-}
-
-// it should have a function to change todos
-function changeTodo(position, newValue){
-    todos[position] = newValue;
-    displayTodos();
-}
-
-// it should have a function to delete todos
-function deleteTodo(position){
-    todos.splice(position,1);
-    displayTodos();
-}
-*/
-
-/*
-// version 3 - moved the todo functions onto the object
-var todoList = {
-  todos: ['item 1', 'item 2', 'item 3'],
-  displayTodos: function(){
-    console.log('My Todos', this.todos);
-  },
-  addTodo: function(todo){
-    this.todos.push(todo);
-    this.displayTodos();
-  },
-  changeTodo: function(position, newValue){
-    this.todos[position] = newValue;
-    this.displayTodos();
-  },
-  deleteTodo: function(position){
-    this.todos.splice(position,1);
-    this.displayTodos();
-  }
-};
-*/
-
-// version 4 - booleans
-// todoList.toggleCompleted should flip the completed property
-// add a new method called toggleCompleted
+// version 4
+// change the addTodo method so it adds objects instead of text to the todos array
 var todoList = {
   todos: [],
   displayTodos: function(){
@@ -68,17 +19,17 @@ var todoList = {
 // change this.todos[position] = newValue; to a property on the object
     this.todos[position].todoText = todoText;
     this.displayTodos();
-  },
+  },  
   deleteTodo: function(position){
     this.todos.splice(position,1);
     this.displayTodos();
-  },
-// add a new method called toggleCompleted
-// use ! to get the opposite of a boolean value
-  toggleCompleted: function(position){
-      var todo = this.todos[position];
-      todo.completed = !todo.completed;
-      this.displayTodos();
   }
 };
 
+// run version 4
+// todoList.addTodo("add first todo");
+// todoList.addTodo("add 2nd todo");
+// todoList.addTodo("add third todo");
+// todoList.changeTodo(1, "change second todo");
+// todoList.addTodo("add thth todo");
+// todoList.changeTodo(3, "add forrth todo");
