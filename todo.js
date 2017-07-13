4042744e37b1596dd9ebbf15b732732dc074217a
Edward Dupre <edwd42@gmail.com>
2017-07-13 17:20:42 +0000

    update todo.js for version 2 - functions

--- todo.js
+++ todo.js
@@ -1,31 +1,27 @@
-// version 1 - arrays
+// version 2 - functions
 var todos = ['item 1', 'item 2', 'item 3']
 
-// it should have a way to display todos
-console.log(todos);
-console.log(todos[0]);
-console.log(todos[1]);
-console.log(todos[2]);
+// it should have a function to display todos
+function displayTodos(){
+    console.log('My todos:', todos);
+}
 
+// it should have a function to add todos
+function addTodo(todo){
+    todos.push(todo);
+    displayTodos();
+}
 
-// it should have a way to add todos
-console.log("adding item 4 and item 5...")
-todos.push('item 4');
-console.log(todos);
-todos.push('item 5');
-console.log(todos);
+// it should have a function to change todos
+function changeTodo(position, newValue){
+    todos[position] = newValue;
+    displayTodos();
+}
 
-// it should have a way to change todos
-console.log("changing item 1...")
-todos[0] = 'item 1 updated';
-console.log(todos);
-
-// it should have a way to delete todos
-console.log("deleting item 1...")
-todos.splice(0,1);
-console.log(todos);
-console.log("deleting item 5...")
-todos.splice(3,1);
-console.log(todos);
+// it should have a function to delete todos
+function deleteTodo(position){
+    todos.splice(position,1);
+    displayTodos();
+}
 
 
