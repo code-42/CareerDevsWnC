2375636386228d26b20cec4393e3c4926ea4d9dd
Edward Dupre <edwd42@gmail.com>
2017-07-13 17:00:40 +0000

    update version 1 bonus.txt

--- todo.js
+++ todo.js
@@ -1,47 +1,31 @@
-/*
+// version 1 - arrays
 var todos = ['item 1', 'item 2', 'item 3']
 
-// it should have a function to display todos
-function displayTodos(){
-    console.log('My todos:', todos);
-}
+// it should have a way to display todos
+console.log(todos);
+console.log(todos[0]);
+console.log(todos[1]);
+console.log(todos[2]);
 
-// it should have a function to add todos
-function addTodo(todo){
-    todos.push(todo);
-    displayTodos();
-}
 
-// it should have a function to change todos
-function changeTodo(position, newValue){
-    todos[position] = newValue;
-    displayTodos();
-}
+// it should have a way to add todos
+console.log("adding item 4 and item 5...")
+todos.push('item 4');
+console.log(todos);
+todos.push('item 5');
+console.log(todos);
 
-// it should have a function to delete todos
-function deleteTodo(position){
-    todos.splice(position,1);
-    displayTodos();
-}
-*/
+// it should have a way to change todos
+console.log("changing item 1...")
+todos[0] = 'item 1 updated';
+console.log(todos);
 
-// version 3 - moved the todo functions onto the object
-var todoList = {
-  todos: ['item 1', 'item 2', 'item 3'],
-  displayTodos: function(){
-    console.log('My Todos', this.todos);
-  },
-  addTodo: function(todo){
-    this.todos.push(todo);
-    this.displayTodos();
-  },
-  changeTodo: function(position, newValue){
-    this.todos[position] = newValue;
-    this.displayTodos();
-  },
-  deleteTodo: function(position){
-    this.todos.splice(position,1);
-    this.displayTodos();
-  }
-};
+// it should have a way to delete todos
+console.log("deleting item 1...")
+todos.splice(0,1);
+console.log(todos);
+console.log("deleting item 5...")
+todos.splice(3,1);
+console.log(todos);
+
 
--- version1_arrays.js
+++ version1_arrays.js
@@ -6,13 +6,13 @@
 // change a todos
 // delete a todo
 
-// store todos
+// it should have a place to store todos
 var todos = ['item1','item2','item3'];
 
-// display todos
+// it should have a way to display todos
 console.log('My todos:', todos);
 
-// add new todos
+// it should have a way to add new todos
 todos.push('item 4');
 console.log('My todos:', todos);
 todos.push('item 5');
@@ -20,13 +20,13 @@ console.log('My todos:', todos);
 todos.push('new todo');
 console.log('My todos:', todos);
 
-// changing the first item
+// it should have a way to change a todo
 todos[0] = 'item 1 updated';
 console.log('My todos:', todos);
 todos[0] = 'changed!';
 console.log('My todos:', todos);
 
-// Deleteing the first item
+// it should have a way to delete a todo
 todos.splice(0,1);
 console.log('My todos:', todos);
 
