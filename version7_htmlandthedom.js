8ebcec5b90b0157951c954abb85dde5e4e1059d5
Edward Dupre <edwd42@gmail.com>
2017-07-17 11:31:17 +0000

    first requirement: there should be a 'Display todos' button and a 'Toggle all' button

---
+++ version7_htmlandthedom.js
@@ -0,0 +1,5 @@
+// v7 requirements
+// there should be a "Display todos" button and a "Toggle all" button
+// clicking "Display todos" should run todoList.displayTodos
+// clicking "Toggle all" should run todoList.toggleAll
+
