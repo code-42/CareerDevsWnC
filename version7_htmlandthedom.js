df6ac35f6ef37db2aa4d6c5d0acf6c4f15ff2382
Edward Dupre <edwd42@gmail.com>
2017-07-17 12:15:20 +0000

    finished video 7

---
+++ version7_htmlandthedom.js
@@ -0,0 +1,26 @@
+// v7 requirements
+// there should be a "Display todos" button and a "Toggle all" button
+// clicking "Display todos" should run todoList.displayTodos
+// clicking "Toggle all" should run todoList.toggleAll
+
+// 1. We want to get access to the display todos button.
+var displayTodosButton = document.getElementById('displayTodosButton');
+console.log(displayTodosButton);
+
+var toggleAllButton = document.getElementById('displayTodosButton');
+console.log(displayTodosButton);
+
+
+// 2. We want to run displayTodos method, when someone clicks the 
+// display todos button
+displayTodosButton.addEventListener('click', function(){
+  todoList.displayTodos();
+})
+
+// clicking "Toggle all" should run todoList.toggleAll
+
+// add event listener for toggleAllButton
+toggleAllButton.addEventListener('click', function(){
+  todoList.toggleAll();
+})
+
