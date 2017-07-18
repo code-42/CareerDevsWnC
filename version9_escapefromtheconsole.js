278bec3495f5407adf4103915e5c1f39a0366b52
Edward Dupre <edwd42@gmail.com>
2017-07-18 12:15:52 +0000

    inserting li elements into the DOM

---
+++ version9_escapefromtheconsole.js
@@ -0,0 +1,10 @@
+// v9 requirements
+// there should be an li element for every todo
+// each li element should contain .todoText
+// each li element should show .completed]
+
+// inserting li elements into the DOM
+var todoLi = document.createElement('li');
+var todosUl = document.querySelector('ul');
+todosUL.appendChild(todoLi);
+
